eb75a31bba052c7b09af73e9ef6134055b998115
Irving Didier Sarmiento De La Cruz <130480598+IrvingDidierRCCL@users.noreply.github.com>
2024-04-07 17:56:19 -0600

    using defer options

--- src/app/dashboard/pages/defer-option/defer-option.component.ts
+++ src/app/dashboard/pages/defer-option/defer-option.component.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
   imports: [
     CommonModule,
+    HeavyLoadersFastComponent,
+    TitleComponent
   ],
   templateUrl: './defer-option.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class DeferOptionComponent { }
