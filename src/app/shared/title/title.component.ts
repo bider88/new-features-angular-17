8b7381a1e12a33116f0ca287b699ad4a9eb53cbd
Irving Didier Sarmiento De La Cruz <130480598+IrvingDidierRCCL@users.noreply.github.com>
2024-04-07 15:04:05 -0600

    add title component with their inputs

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, booleanAttribute, input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
-  templateUrl: './title.component.html',
-  styles: ``
+  imports: [
+    CommonModule
+  ],
+  styles: ``,
+  template: `
+    <h1 class="text-3xl mb-5" [ngClass]="{'shadow': withShadow}">{{ title }}</h1>
+  `
 })
 export class TitleComponent {
+  // Old way to use input (props) - stable
+  @Input({required: true}) title!: string;
+  @Input({transform: booleanAttribute}) withShadow: boolean = false;
 
+  // New approach to use input with signals - developer preview
+  // public title = input.required<string>();
+
+  /**
+   * @memberof TitleComponent
+   * @Eg <app-title title="Some title" withShadow/>
+   */
+  // public withShadow = input(false, {
+  //   transform: (value: boolean|string) => typeof value === 'string' ? value === '' : value
+  // });
 }
