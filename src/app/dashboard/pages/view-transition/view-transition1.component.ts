a433427f294feb0d4989be8618e8e3c2a34f7e04
Irving Didier Sarmiento De La Cruz <130480598+IrvingDidierRCCL@users.noreply.github.com>
2024-04-07 18:40:25 -0600

    add view transtion pages with animate

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -14,9 +14,11 @@ import { TitleComponent } from '@shared/title/title.component';
         srcset="https://picsum.photos/id/237/200/300"
         width="200"
         height="300"
+        style="view-transition-name: hero1;"
       />
-      <div class="bg-blue-500 w-56 h-56">
-
+      <div
+        style="view-transition-name: hero2;"
+        class="bg-blue-500 w-56 h-56">
       </div>
     </section>
   `
--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -8,15 +8,17 @@ import { TitleComponent } from '@shared/title/title.component';
   ],
   template: `
     <app-title title="View transition 2" />
-    <section class="flex justify-start">
+    <section class="flex justify-end">
       <img
         alt="picsum"
         srcset="https://picsum.photos/id/237/200/300"
         width="200"
         height="300"
+        style="view-transition-name: hero1;"
       />
-      <div class="bg-blue-500 w-56 h-56">
-
+      <div
+        style="view-transition-name: hero2;"
+        class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded">
       </div>
     </section>
   `
