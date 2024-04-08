912c41793308511ec131fac8d471721c6e1f39ac
Irving Didier Sarmiento De La Cruz <130480598+IrvingDidierRCCL@users.noreply.github.com>
2024-04-07 20:57:11 -0600

    publishing project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
       routes,
+      withHashLocation(),
       withViewTransitions({
         skipInitialTransition: true
       })
